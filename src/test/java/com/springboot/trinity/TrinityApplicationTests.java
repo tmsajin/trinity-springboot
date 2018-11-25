0282a7f7ee56e3a49bd9d651bcc7759f54e47f1e
sajin <tmsajin@gmail.com>
2018-11-25 11:38:11 +0530

    spring boot

---
+++ src/test/java/com/springboot/trinity/TrinityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.trinity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrinityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
